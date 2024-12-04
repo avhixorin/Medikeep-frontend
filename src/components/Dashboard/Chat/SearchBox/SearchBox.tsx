de60cf11b3b14f1e38f38137446567368531ebf7
avhixorin <avhixorin@gmail.com>
2024-12-04 23:59:50 +0530

    feat(chat): enhance SearchBox with user filtering and improved input handling

--- src/components/Dashboard/Chat/SearchBox/SearchBox.tsx
+++ src/components/Dashboard/Chat/SearchBox/SearchBox.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store/store";
 import { User } from "@/types/types";
-import { Search } from "lucide-react";
 import SearchTile from "./SearchTile";
+import { Input } from "@/components/ui/input";
 
 type SearchBoxProps = {
   setIsSearching: (isSearching: boolean) => void;
@@ -11,6 +11,12 @@ type SearchBoxProps = {
 
 const SearchBox: React.FC<SearchBoxProps> = ({ setIsSearching }) => {
   const { allUsers } = useSelector((state: RootState) => state);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  // Filter users based on the search query
+  const filteredUsers = allUsers.users.filter((user: User) =>
+    user.username.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
     <div className="fixed top-0 left-0 w-full h-full bg-white/10 backdrop-blur z-50 flex items-center justify-center">
@@ -28,19 +34,23 @@ const SearchBox: React.FC<SearchBoxProps> = ({ setIsSearching }) => {
           <h2 className="text-xl font-semibold text-gray-800 dark:text-white mb-4">
             Search
           </h2>
-          <input
+          <Input
             type="text"
-            placeholder="Type to search..."
+            placeholder="Search username..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
             className="w-full px-4 py-2 rounded-md border border-gray-300 dark:border-gray-700 bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-400 transition-all mb-4"
           />
 
           {/* Search Results */}
-          {allUsers.users.length > 0 ? (
+          {filteredUsers.length > 0 ? (
             <div className="max-h-[60vh] overflow-y-auto space-y-2 scrollbar-webkit">
-              {allUsers.users.map((user: User) => (
+              {filteredUsers.map((user: User) => (
                 <SearchTile
                   key={user._id}
-                  profileImage={user.profilePicture ? user.profilePicture : "https://randomuser.me/api/portraits"}
+                  profileImage={
+                    user.profilePicture || "https://randomuser.me/api/portraits"
+                  }
                   username={user.username}
                   role={user.role}
                   onConnect={() => {
