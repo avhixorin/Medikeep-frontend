e660b70c8b5a8eecad544bf64a80033d2c7a5c3f
avhixorin <avhixorin@gmail.com>
2025-01-08 14:02:02 +0530

    feat(sheet): add Sheet component with customizable overlay and content; update styles for better responsiveness

--- src/components/Dashboard/ParentContainer/Dashboard.tsx
+++ src/components/Dashboard/ParentContainer/Dashboard.tsx
@@ -7,9 +7,8 @@ const Dashboard: React.FC = () => {
   return (
     <div className="w-full max-h-[100dvh] flex bg-dashboard2 bg-center bg-no-repeat bg-cover dark:bg-[#0C0C0C]">
       <LeftSidebar />
-      
       <div className="flex-1 bg-transparent p-2 dark:bg-[#0C0C0C]">
-        <main className="w-full h-full overflow-y-auto scrollbar-webkit border border-gray-300 dark:border-gray-700 rounded-md">
+        <main className="w-full h-full overflow-y-auto scrollbar-webkit border border-gray-300 dark:border-gray-700 rounded-md overflow-x-auto">
           <Outlet />
         </main>
       </div>
