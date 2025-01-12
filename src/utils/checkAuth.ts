fe50aff4c2496e744712b7ca5ad442bbff996491
avhixorin <avhixorin@gmail.com>
2025-01-12 22:05:02 +0530

    fix(auth): streamline authentication check by removing unnecessary whitespace

--- src/utils/checkAuth.ts
+++ src/utils/checkAuth.ts
@@ -2,9 +2,7 @@ import store from "../redux/store/store";
 import { redirect } from "react-router-dom";
 
 const checkAuth = () => {
-
   const user = store.getState().auth.user;
-
   if (!user) {
     return redirect('/unauthorized'); 
   }
