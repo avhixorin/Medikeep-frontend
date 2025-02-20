c945a9273ef519d3355cc9a1853e60671bc79d45
avhixorin <avhixorin@gmail.com>
2025-02-20 22:35:01 +0530

    added vercel speedInsights

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react-refresh/only-export-components */
 import { Suspense, lazy } from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
@@ -267,5 +268,6 @@ const router = createBrowserRouter(
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
     <RouterProvider router={router} />
+    <SpeedInsights />
   </>
 );
