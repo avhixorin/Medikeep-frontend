56d96b582a9648775d3995357f422e7d2c8c7197
avhixorin <avhixorin@gmail.com>
2025-02-25 23:55:21 +0530

    feat: add cancel button to upload component with close icon

--- src/utils/Upload.tsx
+++ src/utils/Upload.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FileUpload } from "@/components/ui/file-upload";
+import { XIcon } from "lucide-react";
 
 interface UploadProps {
   cancelUpload: () => void;
@@ -7,11 +8,19 @@ interface UploadProps {
 }
 
 const Upload: React.FC<UploadProps> = ({ cancelUpload, setUploadClicked }) => {
-
   return (
-    <div className="absolute inset-0 flex justify-center items-center backdrop-blur-lg bg-slate-400/30">
-      <div className="max-w-md w-full p-6 rounded-lg">
-        <FileUpload cancelUpload={cancelUpload} setUploadClicked={setUploadClicked} />
+    <div className="absolute inset-0 flex justify-center items-center backdrop-blur-lg bg-slate-400/30 dark:bg-black/5">
+      <button
+        onClick={() => setUploadClicked(false)}
+        className="absolute top-10 px-3 py-3 right-10 rounded-full"
+      >
+        <XIcon className="text-neutral-200" />
+      </button>
+      <div className="max-w-md w-full rounded-lg border-dotted border-neutral-200 dark:border-neutral-700 p-4">
+        <FileUpload
+          cancelUpload={cancelUpload}
+          setUploadClicked={setUploadClicked}
+        />
       </div>
     </div>
   );
