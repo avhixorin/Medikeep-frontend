bec1f4953e07d68c876095b43daa5ca6ae8855bd
avhixorin <avhixorin@gmail.com>
2025-02-28 16:28:04 +0530

    feat: enhance password update screen with loading state and animation

--- src/components/Dashboard/Settings/Security/UpdatePasswordScreen.tsx
+++ src/components/Dashboard/Settings/Security/UpdatePasswordScreen.tsx
@@ -1,22 +1,28 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '@/redux/store/store';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import useUpdate from '@/hooks/useUpdate';
-import toast from 'react-hot-toast';
+import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store/store";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import useUpdate from "@/hooks/useUpdate";
+import toast from "react-hot-toast";
+import { motion } from "framer-motion";
 
-const UpdatePasswordScreen: React.FC<{ onClose: (arg0: boolean) => void }> = ({ onClose }) => {
+const UpdatePasswordScreen: React.FC<{ onClose: (arg0: boolean) => void }> = ({
+  onClose,
+}) => {
+  const [updating, setUpdating] = React.useState(false);
   const user = useSelector((state: RootState) => state.auth.user);
-  const [oldPassword, setOldPassword] = React.useState('');
-  const [newPassword, setNewPassword] = React.useState('');
-  const [confirmPassword, setConfirmPassword] = React.useState('');
+  const [oldPassword, setOldPassword] = React.useState("");
+  const [newPassword, setNewPassword] = React.useState("");
+  const [confirmPassword, setConfirmPassword] = React.useState("");
   const url = import.meta.env.VITE_UPDATE_PASSWORD_URL;
   const { updatePassword } = useUpdate(url);
 
   const handlePasswordUpdate = async () => {
+    setUpdating(true);
     if (newPassword !== confirmPassword) {
-      toast.error('Passwords do not match');
+      toast.error("Passwords do not match");
+      setUpdating(false);
       return;
     }
     if (user?._id) {
@@ -24,24 +30,33 @@ const UpdatePasswordScreen: React.FC<{ onClose: (arg0: boolean) => void }> = ({ 
         await updatePassword(oldPassword, newPassword);
         onClose(false);
       } catch (error) {
-        toast.error('Failed to update password');
-        console.error('Error updating password:', error);
+        toast.error("Failed to update password");
+        console.error("Error updating password:", error);
       }
     } else {
-      toast.error('User ID is not available');
+      toast.error("User ID is not available");
     }
+    setUpdating(false);
   };
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="relative bg-white dark:bg-gray-800 dark:text-gray-200 text-gray-600 rounded-lg p-6 shadow-lg w-96">
+    <div className="fixed inset-0 bg-black/40 backdrop-blur-lg bg-opacity-50 flex items-center justify-center z-50">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.8 }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+        className="relative bg-white dark:bg-black dark:text-gray-200 text-gray-600 rounded-lg p-6 shadow-lg w-96"
+      >
         <button
           onClick={() => onClose(false)}
           className="absolute top-3 right-3 text-gray-600 dark:text-gray-300 hover:text-red-500 dark:hover:text-red-400 transition-colors text-2xl"
         >
           &times;
         </button>
-        <h2 className="text-lg font-semibold text-center mb-4">Change Password</h2>
+        <h2 className="text-lg font-semibold text-center mb-4">
+          Change Password
+        </h2>
         <div className="space-y-4">
           <Input
             type="password"
@@ -65,12 +80,13 @@ const UpdatePasswordScreen: React.FC<{ onClose: (arg0: boolean) => void }> = ({ 
         <Button
           variant="destructive"
           size="sm"
-          className="mt-4 w-full"
+          disabled={updating}
+          className="mt-4 w-full disabled:opacity-50"
           onClick={handlePasswordUpdate}
         >
-          Change Password
+          {updating ? "Updating password..." : "Change Password"}
         </Button>
-      </div>
+      </motion.div>
     </div>
   );
 };
